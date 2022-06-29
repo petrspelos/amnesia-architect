82187da18bfcdfbd555bd0d854abf1f0f68b8a9b
petrspelos <petrspelos@users.noreply.github.com>
2022-06-29 08:51:05 +0200

    Implement Avalonia Code Editor

--- src/AmnesiaArchitect.AvaloniaApp/Views/Documents/TestDocumentView.axaml.cs
+++ src/AmnesiaArchitect.AvaloniaApp/Views/Documents/TestDocumentView.axaml.cs
@@ -1,13 +1,20 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using AvaloniaEdit;
+using AvaloniaEdit.Indentation.CSharp;
 
 namespace AmnesiaArchitect.AvaloniaApp.Views.Documents
 {
     public partial class TestDocumentView : UserControl
     {
+        private readonly TextEditor _textEditor;
+
         public TestDocumentView()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            _textEditor = this.FindControl<TextEditor>("textCode");
+            _textEditor.ShowLineNumbers = true;
+            _textEditor.TextArea.IndentationStrategy = new CSharpIndentationStrategy();
         }
 
         private void InitializeComponent()
